bba0c2fc69bb05f7a1f840d1ebfbc6392851bcb6
Element <414280563@qq.com>
2025-05-01 13:28:43 +0800

    clear  the old codes

--- Notify.hpp
+++ Notify.hpp
@@ -3,30 +3,6 @@
 #include "Log.hpp"
 
 
-EXTERN_C_START
-
-_IRQL_requires_same_
-_IRQL_requires_(PASSIVE_LEVEL)
-NTSTATUS
-InitializeNotify();
-
-
-_IRQL_requires_max_(APC_LEVEL)
-NTSTATUS
-FinalizeNotify();
-
-
-EXTERN_C_END
-
-#if defined(ALLOC_PRAGMA)
-
-#pragma alloc_text(PAGE, InitializeNotify)
-#pragma alloc_text(PAGE, FinalizeNotify)
-
-#endif
-
-
-
 
 class ThreadNotify
 {
